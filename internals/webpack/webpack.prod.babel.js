d5595962a57b553bb1c7bf359ca7af813a6407db
Jun Sun <qingyun95@gmail.com>
2019-11-24 13:19:02 +1100

    correct public path in webpack prod config

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -19,7 +19,7 @@ module.exports = require('./webpack.base.babel')({
   output: {
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].chunk.js',
-    publicPath: '/MediumUs',
+    publicPath: '/MediumUs/',
   },
 
   optimization: {
