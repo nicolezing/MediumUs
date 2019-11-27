33a1758e133d40a1b56c17dd8c95f8c2a318153a
Ying Zhou <nicolezing@gmail.com>
2019-11-27 23:22:30 +1100

    finish all title and subtitle components

--- app/components/ArticleTitle/StyledWrapper.js
+++ app/components/ArticleTitle/StyledWrapper.js
@@ -1,41 +1,29 @@
 import styled from 'styled-components';
+import * as titleStyles from './titleStyles';
 
 const TitleWrapper = styled.div`
-  max-height: 135px;
   display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  margin-bottom: 16px;
-
-  overflow: hidden;
-  text-overflow: ellipsis;
+  flex-flow: column wrap;
+  word-wrap: break-word;
+  ${props => titleStyles[props.type].wrapper}
 `;
 
 const StyledTitle = styled.h1`
-  font-size: 24px;
   font-weight: 600;
-  margin-bottom: 4px;
-
-  letter-spacing: -0.42px;
   color: rgba(0, 0, 0, 0.84);
-  -webkit-line-clamp: 3;
   overflow: hidden;
   display: -webkit-box;
   -webkit-box-orient: vertical;
-  @media screen and (max-width: 767px) {
-    font-size: 21px;
-    -webkit-line-clamp: 4;
-  }
+  margin: 0;
+  ${props => titleStyles[props.type].h1}
 `;
 
 const StyledSubtitle = styled.div`
   overflow: hidden;
-  // max-height: 60px;
   display: -webkit-box;
-  -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
-  line-height: 20px;
   color: rgba(0, 0, 0, 0.54);
+  ${props => titleStyles[props.type].sub}
 `;
 
 export { TitleWrapper, StyledTitle, StyledSubtitle };
--- app/components/ArticleTitle/index.js
+++ app/components/ArticleTitle/index.js
@@ -6,24 +6,28 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TitleWrapper, StyledTitle, StyledSubtitle } from './StyledText';
-function ArticleTitle(props) {
+import _ from 'lodash';
+import { TitleWrapper, StyledTitle, StyledSubtitle } from './StyledWrapper';
+import * as titleStyles from './titleStyles';
+
+function TitleWithSub(props) {
   return (
-    <TitleWrapper>
+    <TitleWrapper {...props}>
       <a href={props.articleLink}>
-        <StyledTitle>{props.title}</StyledTitle>
+        <StyledTitle {...props}>{props.title} </StyledTitle>
       </a>
       <a href={props.articleLink}>
-        <StyledSubtitle>{props.subtitle}</StyledSubtitle>
+        <StyledSubtitle {...props}>{props.subtitle}</StyledSubtitle>
       </a>
     </TitleWrapper>
   );
 }
 
-ArticleTitle.propTypes = {
+TitleWithSub.propTypes = {
   articleLink: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
+  type: PropTypes.oneOf([..._.keys(titleStyles)]).isRequired,
 };
 
-export default ArticleTitle;
+export default TitleWithSub;
