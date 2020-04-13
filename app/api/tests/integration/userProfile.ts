ae286e4cf57b0eade22c67e8d15a7fe041099083
Jun Sun <qingyun95@gmail.com>
2020-04-13 10:56:22 +1000

    add integration test userFollowing

--- app/api/tests/integration/userProfile.ts
+++ app/api/tests/integration/userProfile.ts
@@ -1,4 +1,5 @@
-import { expect, config as chaiConfig } from 'chai';
+import { expect, use as chaiUse } from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
 import { v4 as uuidv4 } from 'uuid';
 import firebaseInit, { getAuth } from '../..';
 import {
@@ -17,13 +18,15 @@ const USER_PASS_2 = 'strong password 2';
 const USER_NAME_1 = 'good name 1';
 const USER_NAME_2 = 'good name 2';
 
+chaiUse(chaiAsPromised);
+
 describe('User', () => {
   it('should be able to update own profile and read others profiles', async () => {
     await firebaseInit();
     expect(await listUserIds()).to.be.empty;
 
     // Signing in non-existent accounts throws.
-    expect(async () => await signIn(USER_EMAIL_1, USER_PASS_1)).to.throw;
+    await expect(signIn(USER_EMAIL_1, USER_PASS_1)).to.be.rejected;
     expect(getAuth().currentUser).to.be.null;
 
     // Signing out out when signed out is no-op.
@@ -33,7 +36,7 @@ describe('User', () => {
     // Accounts are ready to use right after creation.
     const uid1 = await createAccount(USER_EMAIL_1, USER_PASS_1);
     expect(await listUserIds()).to.have.members([uid1]);
-    await signIn(USER_EMAIL_1, USER_PASS_1);
+    // createAccount auto signs in the new account.
     expect(getAuth().currentUser!.uid).to.equal(uid1);
 
     expect((await readProfile(uid1)).name).to.equal(USER_EMAIL_1);
@@ -41,29 +44,31 @@ describe('User', () => {
     expect((await readProfile(uid1)).name).to.equal(USER_NAME_1);
 
     // Must sign out before creating accounts.
-    expect(async () => await createAccount(USER_EMAIL_2, USER_PASS_2)).to.throw;
+    await expect(createAccount(USER_EMAIL_2, USER_PASS_2)).to.be.rejected;
     expect(await listUserIds()).to.have.members([uid1]);
 
     await signOut();
     expect(getAuth().currentUser).to.be.null;
-    expect(async () => await updateProfile({ name: USER_NAME_2 })).to.throw;
+    await expect(updateProfile({ name: USER_NAME_2 })).to.be.rejected;
     expect((await readProfile(uid1)).name).to.equal(USER_NAME_1);
 
     // Email must be unique.
-    expect(async () => await createAccount(USER_EMAIL_1, USER_PASS_2)).to.throw;
+    await expect(createAccount(USER_EMAIL_1, USER_PASS_2)).to.be.rejected;
     expect(await listUserIds()).to.have.members([uid1]);
 
     const uid2 = await createAccount(USER_EMAIL_2, USER_PASS_2);
     expect(await listUserIds()).to.have.members([uid1, uid2]);
-    await signIn(USER_EMAIL_2, USER_PASS_2);
     expect(getAuth().currentUser!.uid).to.equal(uid2);
+
+    // Must sign out before signing in.
+    await expect(signIn(USER_EMAIL_1, USER_PASS_1)).to.be.rejected;
 
     await updateProfile({ name: USER_NAME_2 });
     expect((await readProfile(uid1)).name).to.equal(USER_NAME_1);
     expect((await readProfile(uid2)).name).to.equal(USER_NAME_2);
 
     // Must sign out before signing in.
-    expect(async () => await signIn(USER_EMAIL_1, USER_PASS_1)).to.throw;
+    await expect(signIn(USER_EMAIL_1, USER_PASS_1)).to.be.rejected;
     expect(getAuth().currentUser!.uid).to.equal(uid2);
   });
 });
