3ddc8e0c95989da7fdba6157696818fb804476f0
Jun Sun <qingyun95@gmail.com>
2020-04-18 17:41:36 +1000

    integration script to exit on mocha failure

--- app/api/tests/integration/run.sh
+++ app/api/tests/integration/run.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
+# exit when any command fails
+set -e
+
 for testFile in app/api/tests/integration/*.ts; do
   firebase firestore:delete -P mediumus-dev --all-collections -y
-  mocha --timeout 60000 --full-trace --exit -r ts-node/register "$testFile"
+  mocha --timeout 150000 --full-trace --exit -r ts-node/register "$testFile"
 done
